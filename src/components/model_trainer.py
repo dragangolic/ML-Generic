661d2cee5008215f4bf95d3bd8d72aa944d9f565
Dragan Golic <golicdragan0@gmail.com>
2026-07-02 12:31:18 +0200

    Hyperparam. tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,16 +44,50 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
-            model_predict: dict=evaluate_models(
+
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [5, 7, 9, 11],
+                    #'weights': ['uniform', 'distance'],
+                    #'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "XGB Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [.1, .01, .05],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, .5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
+            model_report: dict = evaluate_models(
                                 X_train=X_train, y_train=y_train, 
                                 X_test=X_test, y_test=y_test, 
-                                models=models)
+                                models=models, param=params)
+            
             # Best model
-            best_model_score = max(sorted(model_predict.values()))
+            best_model_score = max(sorted(model_report.values()))
 
             # Get the best model name
-            best_model_name = list(model_predict.keys())[
-                list(model_predict.values()).index(best_model_score)
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
 
